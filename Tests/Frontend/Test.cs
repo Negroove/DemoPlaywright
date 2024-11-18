2fc1b126febe4b6410a689b6d41111e18ee57ede
Lucas Montoya <lucasmontoya15@hotmail.com>
2024-11-18 14:33:28 -0300

    Agregar archivos de proyecto.

---
+++ Tests/Frontend/Test.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Playwright.NUnit;
+using Microsoft.Playwright;
+
+
+namespace DemoPlaywright.Tests.Frontend;
+
+[Parallelizable(ParallelScope.Self)]
+[TestFixture]
+public class Tests : PageTest
+{
+    [Test]
+    public async Task MyTest()
+    {
+        await Page.GetByPlaceholder("Search").ClickAsync();
+        await Page.GetByPlaceholder("Search").FillAsync("iphone");
+        await Page.GetByRole(AriaRole.Button, new() { Name = "" }).ClickAsync();
+        await Page.GetByText("iPhone", new() { Exact = true }).ClickAsync();
+    }
+
+    [SetUp]
+    public async Task OpenBrowser()
+    {
+        await Page.GotoAsync("https://opencart.abstracta.us/");
+
+    }
+}
